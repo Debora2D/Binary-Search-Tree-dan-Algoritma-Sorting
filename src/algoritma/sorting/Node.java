89e01096bee47625b267db1b39544cfb0f39f9af
Debora Rizky Mustikaningtyas <deborarizky14@gmail.com>
2017-12-13 23:46:40 +0700

    Create Node.java

---
+++ src/algoritma/sorting/Node.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoritma.sorting;
+
+class Node{
+     Node left;
+     Node right;
+     int data;
+ 
+     public Node(int n){
+         left = null;
+         right = null;
+         data = n;
+     }
+     
+    public void setLeft(Node n){
+        left = n;
+    }
+ 
+    public void setRight(Node n){
+        right = n;
+    }
+ 
+    public Node getLeft(){
+        return left;
+    }
+    
+    public Node getRight(){
+        return right;
+    }
+
+    public void setData(int d){
+ 
+        data = d;
+    }
+ 
+    public int getData() {
+        return data;
+    }
+}
