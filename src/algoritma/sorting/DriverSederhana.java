73b0ae22fec2b05b57b13072b5dee4a12d69461a
Debora Rizky Mustikaningtyas <deborarizky14@gmail.com>
2017-12-13 23:44:58 +0700

    Create DriverSederhana.java

---
+++ src/algoritma/sorting/DriverSederhana.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoritma.sorting;
+
+public class DriverSederhana {
+    public static void main(String[] args) {
+        BinarySearchTree bt =new BinarySearchTree();
+        bt.insert(1);
+        bt.insert(2);
+        bt.insert(3);
+        System.out.println(bt.contains(2));
+        
+    }
+}
